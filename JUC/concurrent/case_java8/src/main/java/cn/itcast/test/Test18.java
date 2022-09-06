acfe0b06c948ab693318175d7a48e464c02eedaa
Cai Peishen <peishen.cai@foxmail.com>
2022-09-06 15:14:42 +0800

    当小南线程调用了wait方法时，当前栈帧信息会保存，等其他线程调用了notify方法，小南线程重新去排队，如果有机会拿到锁，会获取上次保存的栈帧信息，代码继续往下执行，我想表达的意思是，如果有wile循环时，从代码层面来看，代码没有跳过本次循环，而是在wait方法进行了等待，等待下次拿到锁，执行wait方法下方的代码

--- JUC/concurrent/case_java8/src/main/java/cn/itcast/test/Test18.java
+++ JUC/concurrent/case_java8/src/main/java/cn/itcast/test/Test18.java
@@ -7,12 +7,28 @@ public class Test18 {
     static final Object lock = new Object();
     public static void main(String[] args) {
 
-        synchronized (lock) {
-            try {
-                lock.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                lock.notifyAll();
             }
-        }
+        }, "t1").start();
+
+            synchronized (lock) {
+                try {
+                    System.out.println("---1");
+                    lock.wait();
+                    System.out.println("---2");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
     }
 }
