afc3c8b337d3077bbf774c030f598563a42dff79
Cai Peishen <peishen.cai@foxmail.com>
2022-09-13 11:00:30 +0800

    AtomicMarkableReference是将一个boolean值作是否有更改的标记，本质就是它的版本号只有两个，true和false，修改的时候在这两个版本号之间来回切换，这样做并不能解决ABA的问题，只是会降低ABA问题发生的几率而已

--- JUC/concurrent/case_java8/src/main/java/cn/itcast/test/Test38.java
+++ JUC/concurrent/case_java8/src/main/java/cn/itcast/test/Test38.java
@@ -6,6 +6,10 @@ import java.util.concurrent.atomic.AtomicMarkableReference;
 
 import static cn.itcast.n2.util.Sleeper.sleep;
 
+/**
+ * AtomicMarkableReference是将一个boolean值作是否有更改的标记，本质就是它的版本号只有两个，true和false，
+ * 修改的时候在这两个版本号之间来回切换，这样做并不能解决ABA的问题，只是会降低ABA问题发生的几率而已
+ */
 @Slf4j(topic = "c.Test38")
 public class Test38 {
     public static void main(String[] args) throws InterruptedException {
@@ -22,9 +26,18 @@ public class Test38 {
             bag.setDesc("空垃圾袋");
             ref.compareAndSet(bag, bag, true, false);
             log.debug(bag.toString());
+            sleep(0.5);
         },"保洁阿姨").start();
 
-        sleep(1);
+        new Thread(() -> {
+            log.debug("start...");
+            bag.setDesc("装满了垃圾");
+            ref.compareAndSet(bag, bag, false, true);
+            log.debug(bag.toString());
+            sleep(0.5);
+        },"保洁阿姨").start();
+
+        sleep(1.5);
         log.debug("想换一只新垃圾袋？");
         boolean success = ref.compareAndSet(prev, new GarbageBag("空垃圾袋"), true, false);
         log.debug("换了么？" + success);
